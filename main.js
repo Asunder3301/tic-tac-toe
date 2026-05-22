6e3a17def3c7ab55d2ddea1e855725c46e091110
Asunder <trashemail3301@gmail.com>
2026-05-22 11:29:24 -0500

    Allow winner to be determined

--- main.js
+++ main.js
@@ -50,5 +50,18 @@ const gameController = (() => {
 
     const switchTurns = () => { activePlayerIndex = activePlayerIndex === 0 ? 1 : 0; };
 
-    return { startGame, getActivePlayer, switchTurns };
+    const checkWin = () => {
+        const currentBoard = board.getGameboard();
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6] 
+        ]
+
+        return winPatterns.some(pattern => {
+            return pattern.every(index => currentBoard[index] === getActivePlayer().marker);
+        })
+    }
+
+    return { startGame, getActivePlayer, checkWin};
 })();
